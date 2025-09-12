806cdfdbc56d6eef1179826e660c8cffa9334417
Brylle Cortes <brylle303@gmail.com>
2025-09-12 20:20:43 +0800

    Finalized lab exercise 1

--- src/labExercise1/distanceBetweenTwoPoints.java
+++ src/labExercise1/distanceBetweenTwoPoints.java
@@ -5,13 +5,27 @@ import java.util.Scanner;
 public class distanceBetweenTwoPoints {
 
     public static void main(String[] args) {
-        // Program
-        int x1, x2;
-        int y1, y2;
-        double tempResult, finalResult;
+        // Program that calculates the distance between two given points
+        Scanner scan = new Scanner(System.in);
 
+        double x1, x2;
+        double y1, y2;
+        double finalResult;
+
+        // Obtain input
         System.out.println("[Distance Calculator]");
         System.out.print("Enter x1 and y1: ");
+        x1 = scan.nextDouble();
+        y1 = scan.nextDouble();
+
+        System.out.print("Enter x2 and y2: ");
+        x2 = scan.nextDouble();
+        y2 = scan.nextDouble();
+
+        // Calculate distance
+        finalResult = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
+
+        System.out.printf("The distance between two points is: %.3f", finalResult);
 
 
 
