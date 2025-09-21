04fe613e3532762dea4d9854f435696794decbef
Brylle Cortes <brylle303@gmail.com>
2025-09-21 19:15:55 +0800

    Finalized LE 2

--- src/labExercise2/decToHex.java
+++ src/labExercise2/decToHex.java
@@ -14,8 +14,8 @@ public class decToHex {
 		
 		char[] hexaValues = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 		
-		System.out.println("[Decimal to Hexa]");
-		System.out.println("Enter a decimal number: ");
+		System.out.print("[Decimal to Hexa]");
+		System.out.print("Enter a decimal number: ");
 		
 		decimalInput = scan.nextInt();
 		scan.close();
