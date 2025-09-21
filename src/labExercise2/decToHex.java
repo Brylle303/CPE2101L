5a96d1eafe72273b842a8d0ada25db0b6b538c3e
Brylle Cortes <brylle303@gmail.com>
2025-09-21 19:13:39 +0800

    Finished 3rd activity

---
+++ src/labExercise2/decToHex.java
@@ -0,0 +1,33 @@
+package labExercise2;
+
+import java.util.Scanner;
+
+public class decToHex {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String hexaOutput = "";
+		int decimalInput;
+		int remainder;
+		
+		
+		char[] hexaValues = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+		
+		System.out.println("[Decimal to Hexa]");
+		System.out.println("Enter a decimal number: ");
+		
+		decimalInput = scan.nextInt();
+		scan.close();
+		
+		// Loop for successive division
+		while(decimalInput > 0) {
+			
+			remainder = decimalInput % 16;
+			hexaOutput = hexaValues[remainder] + hexaOutput;
+			decimalInput /= 16;
+		}
+		
+		System.out.println("Hexadecimal Equivalent: " + hexaOutput);
+	}
+}
