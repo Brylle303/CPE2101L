00c29cbeb0bb2c3888b0fcb00f44580ff3a8b9de
Brylle Cortes <brylle303@gmail.com>
2025-10-07 15:56:48 +0800

    Update exercise 3.1

--- src/labExercise3_1/Person.java
+++ src/labExercise3_1/Person.java
@@ -2,4 +2,50 @@ package labExercise3_1;
 
 public class Person {
 
+	private String name;
+	private int age;
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setNameAndAge(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public boolean isEqual(Object obj) {
+		
+		if(this == obj) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean isSameName(Object obj) {
+		if(this.name == obj) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	
 }
