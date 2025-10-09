555cc0e109ae3a4cba0344066a331901117b8c41
Brylle Cortes <brylle303@gmail.com>
2025-10-09 21:08:34 +0800

    Added and updated test program

--- src/labExercise3_3/GradeDistribution.java
+++ src/labExercise3_3/GradeDistribution.java
@@ -2,50 +2,111 @@ package labExercise3_3;
 
 public class GradeDistribution {
 	
-	private int A, B, C, D, F;
-	private int totalGrades;
+	private double gradeA, gradeB, gradeC, gradeD, gradeF;
 	
 	// Methods for setting each grade
-	public void setNumberA(int A) {
-		this.A = A;
+	public void setNumberA(double gradeA) {
+		this.gradeA = gradeA;
 	}
-	public void setNumberB(int B) {
-		this.B = B;
+	public void setNumberB(int gradeB) {
+		this.gradeB = gradeB;
 	}
-	public void setNumberC(int C) {
-		this.C = C;
+	public void setNumberC(int gradeC) {
+		this.gradeC = gradeC;
 	}
-	public void setNumberD(int D) {
-		this.D = D;
+	public void setNumberD(int gradeD) {
+		this.gradeD = gradeD;
 	}
-	public void setNumberF(int F) {
-		this.F = F;
+	public void setNumberF(int gradeF) {
+		this.gradeF = gradeF;
 	}
 	
 	// Methods for reading each grade
 	public int readGradeA() {
-		return A;
+		return (int) gradeA;
 	}
 	public int readGradeB() {
-		return B;
+		return (int) gradeB;
 	}
 	public int readGradeC() {
-		return C;
+		return (int) gradeC;
 	}
 	public int readGradeD() {
-		return D;
+		return (int) gradeD;
 	}
 	public int readGradeF() {
-		return F;
+		return (int) gradeF;
 	}
 	
 	// Total number of grades
-	
-	public int totalGrades(int totalGrades) {
-		this.totalGrades = totalGrades;
-		return this.totalGrades;
+	public double totalGrades() {
+		return gradeA + gradeB + gradeC + gradeD + gradeF;
 	}
 	
+	public double percentageOfGradeA() {
+		return  Math.round((gradeA / totalGrades()) * 100);
+	}
+	public int percentageOfGradeB() {
+		return (int) Math.round((gradeB / totalGrades()) * 100);
+	}
+	public int percentageOfGradeC() {
+		return (int) Math.round((gradeC / totalGrades()) * 100);
+	}
+	public int percentageOfGradeD() {
+		return (int) Math.round((gradeD / totalGrades()) * 100);
+	}
+	public int percentageOfGradeF() {
+		return (int) Math.round((gradeF / totalGrades()) * 100);
+	}
+	
+	
+	public void drawGraph() {
+		int a = 0;
+		
+		for(int i=0;i <= 50; i++) {
+			if(i % 5 != 0) {
+				
+				System.out.print(" ");
+			}
+			else {
+				System.out.print(a);
+				a++;
+			}
+		}
+		System.out.println();
+		
+		for(int i=0;i <= 50; i++) {
+			if(i % 5 != 0) {
+				
+				System.out.print(" ");
+			}
+			else {
+				System.out.print("|");
+			}
+		}
+		System.out.println();
+		for(int i=0;i < 50; i++) {
+			System.out.print("*");
+		}
+		System.out.println("\n");
+		
+		// Drawing the asterisk for each grade
+		for(int i=0;i < Math.round((percentageOfGradeA() / 2)); i++) {
+			System.out.print("*");
+		} System.out.print(" [A]\n");
+		for(int i=0;i < Math.round((percentageOfGradeB() / 2)); i++) {
+			System.out.print("*");
+		} System.out.print(" [B]\n");
+		for(int i=0;i < Math.round((percentageOfGradeC() / 2)); i++) {
+			System.out.print("*");
+		} System.out.print(" [C]\n");
+		for(int i=0;i < Math.round((percentageOfGradeD() / 2)); i++) {
+			System.out.print("*");
+		} System.out.print(" [D]\n");
+		for(int i=0;i < Math.round((percentageOfGradeF() / 2)); i++) {
+			System.out.print("*");
+		} System.out.print(" [F]\n");
+	}
 	
 }
 
---
+++ src/labExercise3_3/GradeTest.java
@@ -0,0 +1,17 @@
+package labExercise3_3;
+
+public class GradeTest {
+
+	public static void main(String[] args) {
+		GradeDistribution myGrades = new GradeDistribution();
+		
+		myGrades.setNumberA(10);
+		myGrades.setNumberB(4);
+		myGrades.setNumberC(6);
+		myGrades.setNumberD(2);
+		myGrades.setNumberF(1);
+		myGrades.drawGraph();
+		
+	}
+
+}
