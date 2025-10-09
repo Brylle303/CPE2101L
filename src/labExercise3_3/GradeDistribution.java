cb6a77a866b98adee1a1d20651b6007150eb9fe3
Brylle Cortes <brylle303@gmail.com>
2025-10-09 17:48:28 +0800

    Updated the class in exercise 3.3

--- src/labExercise3_3/GradeDistribution.java
+++ src/labExercise3_3/GradeDistribution.java
@@ -1,5 +1,51 @@
 package labExercise3_3;
 
 public class GradeDistribution {
-
+	
+	private int A, B, C, D, F;
+	private int totalGrades;
+	
+	// Methods for setting each grade
+	public void setNumberA(int A) {
+		this.A = A;
+	}
+	public void setNumberB(int B) {
+		this.B = B;
+	}
+	public void setNumberC(int C) {
+		this.C = C;
+	}
+	public void setNumberD(int D) {
+		this.D = D;
+	}
+	public void setNumberF(int F) {
+		this.F = F;
+	}
+	
+	// Methods for reading each grade
+	public int readGradeA() {
+		return A;
+	}
+	public int readGradeB() {
+		return B;
+	}
+	public int readGradeC() {
+		return C;
+	}
+	public int readGradeD() {
+		return D;
+	}
+	public int readGradeF() {
+		return F;
+	}
+	
+	// Total number of grades
+	
+	public int totalGrades(int totalGrades) {
+		this.totalGrades = totalGrades;
+		return this.totalGrades;
+	}
+	
+	
 }
+
